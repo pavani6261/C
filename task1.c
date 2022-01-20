e07038b9ac05288fd493ea4f8be6dc9304b405d9
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-20 13:17:31 +0530

    ASCII with for Loop

--- task1.c
+++ task1.c
@@ -1,10 +1,18 @@
+
 #include<stdio.h>
 void main()
 {
+    int i;
     char chr;
-    printf("enter a char");
-    scanf("%c",&chr);
-    printf("\n entered value is %c",chr);
-    printf("\n ASCII value is %d",chr);
+    //printf("enter a char");
+    //scanf("%c",&chr);
+    //printf("\n entered value is %c",chr);
+    //printf("\n ASCII value is %d",chr);
+    for(i=0;i<255;i++)
+        {
+
+            printf("%c - ASCII value of %d\n",i,i);
+        }
 }
+
 
