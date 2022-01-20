b0c92933853c015af54d25c9c9df1f31d453e723
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-20 12:39:21 +0530

    ASCII code program

---
+++ task1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char chr;
+    printf("enter a char");
+    scanf("%c",&chr);
+    printf("\n entered value is %c",chr);
+    printf("\n ASCII value is %d",chr);
+}
+
