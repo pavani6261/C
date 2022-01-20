42f0be73e4a04cff52ecb622cf2044087f5c5da7
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-20 14:16:16 +0530

    switch case for printing month

---
+++ task4.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    switch(i)
+    {
+    case '1':
+        printf("%d is JANUARY",i);
+        break;
+    case '2':
+        printf("%d is FEBRUARY",i);
+        break;
+    case '3':
+        printf("%d is MARCH",i);
+        break;
+    case '4':
+        printf("%d is APRIL",i);
+        break;
+    case '5':
+        printf("%d is MAY",i);
+        break;
+    case '6':
+        printf("%d is JUNE",i);
+        break;
+    case '7':
+        printf("%d is JULY",i);
+        break;
+    case '8':
+        printf("%d is AUGUST",i);
+        break;
+    case '9':
+        printf("%d is SEPTEMBER",i);
+        break;
+    case '10':
+        printf("%d is OCTOBER",i);
+        break;
+    case '11':
+        printf("%d is NOVEMBER",i);
+        break;
+    case '12':
+        printf("%d is DECEMBER",i);
+        break;
+    default:
+        printf("it is not a month");
+    }
+}
