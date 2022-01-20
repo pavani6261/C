942fa79152515fb21dc2f9ff3c5eefeb951e7e07
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-20 14:30:02 +0530

    looping to find smallest of given numbers

--- task3.c
+++ task3.c
@@ -2,11 +2,10 @@
 #include<stdio.h>
 void main()
 {
-    int i;
-    int n;
+    int i,n;
     printf("enter n values\n");
     scanf("%d",&n);
-    int a[n];
+    int a[10];
     printf("enter a values\n");
     for(i=0;i<n;i++){
     scanf("%d",&a[i]);
