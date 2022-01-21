c0902a49f000d3ba495ae2f4e6f88ce7e7ede0e2
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-21 17:05:20 +0530

    searching the a string in list of string

---
+++ string.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char strs[50][50];
+    int n,i,flag=0;
+    //printf("%s",st1);
+    //printf("\n length of the string is:%d\n",strlen(st1));
+    //printf("\nstring:%s",strstr(st1,"come"));
+    printf("enter number of strings:\n");
+    scanf("%d",&n);
+    printf("enter the strings:\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%s",&strs[i]);
+    }
+    char st2[50];
+    printf("enter the string for search:\n");
+    scanf("%s",&st2);
+    for(i=0;i<n;i++)
+    {
+    if(strcmp(strs[i],st2)==0)
+        {
+           printf("string 2 is in string 1");
+        }
+    }
+        printf("string 2 is not in string 1");
+
+}
+
