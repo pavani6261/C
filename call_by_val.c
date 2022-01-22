d4f28af9c70ffe27ec393634a1d3627af9387e00
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-22 13:28:44 +0530

    To swapping of values using call by value function

---
+++ call_by_val.c
@@ -0,0 +1,20 @@
+/*
+#include<stdio.h>
+void main()
+{
+    int x;
+    int y;
+    printf("enter 2 numbers to be swapped \n");
+    scanf("%d%d",&x,&y);
+    //printf("x= %d,y= %d\n",x,y);
+    //printf("x+y=%d\n",call_val(x,y));
+    printf("before swapping x=%d,y=%d\n",x,y);
+    call_val(y,x);
+
+}
+int call_val(int x, int y)
+{
+    printf("after swapping x=%d, y=%d\n",x,y);
+    return 0;
+}
+*/
