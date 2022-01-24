751933c531106995647d401a348cf87e7abe910b
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-24 16:28:28 +0530

    Add files via upload

--- rev_elm_array.c
+++ rev_elm_array.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 void main()
 {
-    int a[9]={1,2,3,4,5,6,7,8,9};
+    int a[10]={0,1,2,3,4,5,6,7,8,9};
     int *pt,*pt2;
     pt=a;
     pt2=a;
     int i;
     printf("array elements are:\n");
-    for(i=0;i<9;i++)
+    for(i=0;i<10;i++)
     {
          printf("%d",a[i]);
     }
     printf("\n reverse of array elements are:\n");
-    for(int i=8;i>=0;i--)
+    for(int i=9;i>=0;i--)
     {
         printf("%d",a[i]);
     }
     printf("\n reverse of array elements using pointers are:\n");
-    for(i=0;i<8;i++)
+    for(i=0;i<9;i++)
     {
          pt2++;
     }
@@ -25,11 +25,11 @@ void main()
     {
         *pt=*pt + *pt2;
         *pt2=*pt - *pt2;
-        *pt=*pt-*pt2;
+        *pt=*pt - *pt2;
         pt2--;
         pt++;
     }
-    for(i=0;i<9;i++)
+    for(i=0;i<10;i++)
     {
         printf("%d",a[i]);
     }
