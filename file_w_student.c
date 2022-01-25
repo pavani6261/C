b9018eb6f73a31f97d32e61a2ff476bf6df17aa5
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-01-25 12:40:42 +0530

    using structures in files
    records 5 student documents are displayed

--- file_w_student.c
+++ file_w_student.c
@@ -5,7 +5,7 @@ struct student
     int id,age;
     char name[20];
     char course[20];
-}st;
+}st[5];
 void main()
 {
     FILE *sfptr;
@@ -16,27 +16,30 @@ void main()
         printf("failed to create file\n");
         exit(1);
     }
-        printf("enter student details:\n");
+    for(int i=0;i<5;i++)
+    {
+        printf("enter student details of %d:\n",i);
         printf("enter student Id\n");
-        scanf("%d",&st.id);
+        scanf("%d",&st[i].id);
         printf("enter student age\n");
-        scanf("%d",&st.age);
+        scanf("%d",&st[i].age);
         printf("enter student name\n");
-        scanf("%s",&st.name);
+        scanf("%s",&st[i].name);
         printf("enter student course\n");
-        scanf("%s",&st.course);
-        fwrite(&st, sizeof(struct student), 1, sfptr);
-
+        scanf("%s",&st[i].course);
+        fwrite(&st[i], sizeof(struct student), 1, sfptr);
+    }
     fclose(sfptr);
     if(fopen("student_details.txt","r")==NULL)
     {
         printf("ERROR ! \n");
         exit(1);
     }
-
-        fread(&st, sizeof(struct student), 1, sfptr);
-        printf("student details are : \n");
-        printf("Id = %d\tAge = %d\tName = %s\tCourse = %s\n",st.id,st.age,st.name,st.course);
-
+    for(int i=0;i<5;i++)
+    {
+        fread(&st[i], sizeof(struct student), 1, sfptr);
+        printf("student details of %d record are : \n",i);
+        printf("Id = %d\tAge = %d\tName = %s\tCourse = %s\n",st[i].id,st[i].age,st[i].name,st[i].course);
+    }
     fclose(sfptr);
 }
